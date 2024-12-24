5fa3c17a4eeea7f879b25c1f9da6a55207f23852
Erick Benitez <erickbenitez336@gmail.com>
2024-12-24 02:15:54 -0500

    Only run tests pre-push

--- scripts/install_hooks.sh
+++ scripts/install_hooks.sh
@@ -2,6 +2,11 @@
 set -e
 
 echo "Installing Git hooks..."
+
 cp .githooks/pre-commit .git/hooks/pre-commit
+cp .githooks/pre-push .git/hooks/pre-push
+
 chmod +x .git/hooks/pre-commit
+chmod +x .git/hooks/pre-push
+
 echo "Pre-commit hook installed!"
