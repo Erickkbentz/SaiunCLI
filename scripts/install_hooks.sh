54063ec7d35a4c75b7e68ac7894c74408a10c2a2
Erick Benitez <erickbenitez336@gmail.com>
2024-12-24 02:13:37 -0500

    change name of dir

--- scripts/install_hooks.sh
+++ scripts/install_hooks.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+set -e
+
 echo "Installing Git hooks..."
-cp .hooks/pre-commit .git/hooks/pre-commit
+cp .githooks/pre-commit .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
 echo "Pre-commit hook installed!"
