b9529408ab8fd1989fa3c585934283b1c7fc073c
Erickkbentz <erickbenitez336@gmail.com>
2025-02-02 20:04:08 -0800

    Remove unused classes

--- src/auracli/context.py
+++
@@ -1,9 +0,0 @@
-class AuraContext:
-    def __init__(
-        self,
-        **kwargs,
-    ):
-        self.__dict__.update(kwargs)
-
-    def __repr__(self):
-        return f"Context({self.__dict__})"
